e324b5b11a304f8548daaa0628707160246f991f
anakoisaac <anakoisaac@gmail.com>
2023-07-26 10:07:27 +0100

    Added dynamic posts loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	$('.blog-archives').on('click', 'a', function(e){
 		e.preventDefault();
-		alert("Displaying post number " + $(this).attr('data-post-id'));
+		let postId = $(this).attr('data-post-id');
+		$("#dynamic-post-wrapper").load('/blog-posts/post-' + postId + '.html');
+		// alert("Displaying post number " + $(this).attr('data-post-id'));
 	});
 });
